52828f5c800c653f521138535ed19e89b901ed3c
fonzroevykjunsay <60122963+fonzroevykjunsay@users.noreply.github.com>
2020-09-05 17:48:44 +0800

    Add files via upload

---
+++ ITL303/index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+  
+<link rel="stylesheet" type="text/css" href="css/css.css">
+<body>
+<div class="page">
+  <nav class="navbar">
+    <button class="navbar-item">Register</button>
+    <button class="navbar-item">Sign in</button>
+
+  </nav>
+  <section class="logo">
+    <div class="logo-item-head">
+    <div class="logo-item-mouth">
+    </div>
+  </div>
+    <div class="eye-left"> 
+    </div>
+    <div class="eye-right">
+    </div>
+
+    <div class="logo-item">Cyanide and Hapiness</div>
+    </section>
+  <div class="page-body">
+    <section class="sidebar">
+      <button class="sidebar-item">Home</button>
+      <button class="sidebar-item">Store</button>
+      <button class="sidebar-item">Comics</button>
+      <button class="sidebar-item">Short videos</button>
+      <button class="sidebar-item">News</button>
+      
+      </section>
+
+
+
+    </section>
+       
+    <section class="columns">
+        <article class="column-item">RECENT COMICS AND SHORT VIDEOS</article>
+        <article class="column-item">BEST COMICS OF THE WEEK</article>
+        <article class="column-item">BEST SHORT VIDEOS OF THE WEEK</article>
+    </section>
+  </div>
+</div>
+</div>
+</body>
+</head>
+</html>

9d919038928c7d254d52699f2455f64020543839
fonzroevykjunsay <60122963+fonzroevykjunsay@users.noreply.github.com>
2020-10-01 01:51:52 +0800

    Update index.php

--- ITL303/index.php
+++ ITL303/index.php
@@ -1,48 +1,115 @@
 <html>
 <head>
-  
-<link rel="stylesheet" type="text/css" href="css/css.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <body>
 <div class="page">
   <nav class="navbar">
     <button class="navbar-item">Register</button>
     <button class="navbar-item">Sign in</button>
-
-  </nav>
-  <section class="logo">
-    <div class="logo-item-head">
-    <div class="logo-item-mouth">
+    <div class="anim"></div>
+</nav>
+  <div class="logo">
+      <figure class="face">
+        <span class="eyes">
+          <span class="eye"></span>
+          <span class="eye"></span>
+        </span>
+        <span class="mouth teeth tounge">
+        </span>
+      </figure>
+    <div class="logo-item">Cyanide and Happiness</div>
     </div>
-  </div>
-    <div class="eye-left"> 
+    <div class="columns">Meet the characters
+    <div class="column-item">
+    <div class="item1">RED
+    <div class="red angry">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth">
+        </span>
+      </figure>
     </div>
-    <div class="eye-right">
     </div>
-
-    <div class="logo-item">Cyanide and Hapiness</div>
-    </section>
-  <div class="page-body">
-    <section class="sidebar">
-      <button class="sidebar-item">Home</button>
-      <button class="sidebar-item">Store</button>
-      <button class="sidebar-item">Comics</button>
-      <button class="sidebar-item">Short videos</button>
-      <button class="sidebar-item">News</button>
-      
-      </section>
-
-
-
-    </section>
-       
-    <section class="columns">
-        <article class="column-item">RECENT COMICS AND SHORT VIDEOS</article>
-        <article class="column-item">BEST COMICS OF THE WEEK</article>
-        <article class="column-item">BEST SHORT VIDEOS OF THE WEEK</article>
-    </section>
-  </div>
+    <div class="item2">BROWN</div>
+    <div class="brown speechless">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth">
+        </span>
+      </figure>
+    </div>
+    <div class="item3">YELLOW</div>
+    <div class="yellow smile">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth">
+        </span>
+      </figure>
+    </div>
+    <div class="item4">GREEN</div>
+    <div class="green laugh">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth tounge">
+        </span>
+      </figure>
+    </div>
+    <div class="item5">BLUE</div>
+    <div class="blue sad">
+      <figure class="face">
+        <span class="eyes">
+            <span class="eye"></span>
+            <span class="eye"></span>
+        </span>
+        <span class="mouth tounge">
+        </span>
+      </figure>
+    </div>
 </div>
 </div>
+<div class="columns">Watch our for our episodes!
+    <div class="column-item"></div>
+    <div class="column-item"></div>
+    <div class="column-item"></div>
+    <div class="column-item"></div>
+    <div class="column-item"></div>
+    <div class="column-item"></div>
+</div>
+<ul class="bubbles">
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
 </body>
 </head>
 </html>
